9dcdcdd8e114d90eb1628f8cb41c773e0f119603
leon <leonsk32@gmail.com>
2019-09-02 03:36:58 +0900

    Round 情報も作者のみに表示

--- src/components/HagetakaRoundInfo.vue
+++ src/components/HagetakaRoundInfo.vue
@@ -1,11 +1,9 @@
 <template>
-  <b-alert variant="info" show align="left" id="info">
-    <div v-if="isAuthor">
-      <router-link v-bind:to="'/hagetaka/rounds/' + roundId">投票</router-link>
-      |
-      <router-link v-bind:to="'/hagetaka/rounds/' + roundId + '/result'">試合結果</router-link>
-      <br>
-    </div>
+  <b-alert variant="info" show align="left" id="info" v-if="isAuthor">
+    <router-link v-bind:to="'/hagetaka/rounds/' + roundId">投票</router-link>
+    |
+    <router-link v-bind:to="'/hagetaka/rounds/' + roundId + '/result'">試合結果</router-link>
+    <br>
     <b-badge>Round ID</b-badge> {{roundId}}<br>
     <b-badge>Round Name</b-badge> {{roundName}}<br>
   </b-alert>
