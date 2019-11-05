8f3d3649bdba4f75b7b19e885eed73cea91af452
leon <leonsk32@gmail.com>
2019-11-06 00:57:59 +0900

    ルールを追記、ユーザ名の制限を解除

--- src/views/Hagetaka.vue
+++ src/views/Hagetaka.vue
@@ -5,6 +5,13 @@
       v-bind:roundName="round.roundName"
       v-bind:isAuthor="isAuthor"
     />
+    <b-card title="ルール" style="margin: 20px">
+      <ul style="text-align:left">
+        <li>１人１回、<span style="color: red; ">{{round.minValue}}</span> 以上 <span style="color: red; ">{{round.maxValue}}</span> 以下の数字を投票することが出来ます</li>
+        <li>最も大きな数字を投票した人の勝利です</li>
+        <li>ただし、他の人と同じ数字を投票してしまった場合、無条件で負けとなります</li>
+      </ul>
+    </b-card>
     <b-card
       title="投票"
       style="margin: 20px"
@@ -112,7 +119,8 @@ export default {
   computed: {
     isValidName () {
       if (this.name === '') return null
-      return Validator.isAlphabeticalNumerical(this.name)
+      // return Validator.isAlphabeticalNumerical(this.name)
+      return true
     },
 
     isValidValue () {
